d06e1d5eb3091f6134b91822bac45e538c8a9c95
Doug Hodson <doug@sidechannel.net>
2020-04-15 00:23:55 -0400

    updated strings example

--- strings.rs
+++ strings.rs
@@ -2,8 +2,9 @@
 fn main() {
    let question = "How are you ?";
    let person: String = "Bob".to_string();
-   let namaste = String::from("\pi");
+   let tau = String::from("τ from LaTeX says");
    
-   println!("{}! {} {}", namaste, question, person);
+   println!("{}! {} {}", tau, question, person);
 }
+
 
